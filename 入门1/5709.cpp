59b8b9ae0e30a4faca53fb7983f7fce6b02424c2
LiZeCheng-tony <Li2763404373@hotmail.com>
2020-08-11 18:32:42 +0800

    5709

--- 入门1/5709.cpp
+++ 入门1/5709.cpp
@@ -1,16 +1,21 @@
+/*=============================================================================
+# 
+#LiZeCheng
+#L2763404373@hotmail.com
+#
+# Last modified: 2020-08-11 18:31
+=============================================================================*/
 #include <iostream>
 #include <stdio.h>
 #include <string>
 using namespace std;
 int main() {
-  int a, b, c;
-    
-  int d;
-  cin >> a >> b >> c;
-  if (c == 0) {
-    cout << a;
-  } else {
-    cout << int((a * b - c) / 10);
-  }
-  return 0;
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (c == 0) {
+        cout << a;
+    } else {
+        cout << int((a * b - c) / 10);
+    }
+    return 0;
 }
